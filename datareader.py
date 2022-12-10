714617835665bebb9a5a6212c6cf77b632f1f56a
Dorijan Cirkveni <97680109+MousePotatoDoesStuff@users.noreply.github.com>
2022-12-10 19:48:50 +0100

    Create datareader.py

---
+++ datareader.py
@@ -0,0 +1,18 @@
+from event_handling import *
+
+class DataReader:
+    def __init__(self,filepath):
+        F=open(filepath,'rb')
+        self.data=pickle.load(F)
+        self.current=self.data
+        F.close()
+        return
+    def list
+
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()
