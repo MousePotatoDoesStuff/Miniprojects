972cc002f30a538a1e42033bfe9fe85849dc4822
Dorijan Cirkveni <97680109+MousePotatoDoesStuff@users.noreply.github.com>
2023-07-22 11:22:43 +0200

    testing

--- smaller projects/logic_gate_finder.py
+++ smaller projects/logic_gate_finder.py
@@ -1,54 +1,63 @@
-tuple_to_bin = lambda T:int(''.join([str(int(e))for e in T]),2)
-bin_to_tuple = lambda b,n:[
-    lambda b:(0,),
-    lambda b:(b,),
-    lambda b:(list(bin_to_tuple(b//2,n-1))+[b%2])
-][(n>0)+(n>1)](b)
+tuple_to_bin = lambda T: int(''.join([str(int(e)) for e in T]), 2)
+bin_to_tuple = lambda b, n: [
+    lambda b: (0,),
+    lambda b: (b,),
+    lambda b: (list(bin_to_tuple(b // 2, n - 1)) + [b % 2])
+][(n > 0) + (n > 1)](b)
 
-def process(gate,data,check=True):
-    if check and len(gate)<(1<<len(data)):
+
+def process(gate, data, check=True):
+    if check and len(gate) < (1 << len(data)):
         raise Exception("Too many inputs!")
-    b=tuple_to_bin(data)
+    b = tuple_to_bin(data)
     return gate[b]
 
-def generate_input(gate,data):
-    if len(gate)<(1<<len(data)):
-        raise Exception("Too many inputs!")
-    M=[data]
+
+def generate_perms(data):
+    M = [data]
     for i in range(len(data)):
-        if M[0][i] in [0,1]:
+        if M[0][i] in [0, 1]:
             continue
-        Y=[]
+        Y = []
         for E in M:
-            X=list(E)
-            X[i]=0
+            X = list(E)
+            X[i] = 0
             Y.append(tuple(X))
-            X[i]=1
+            X[i] = 1
             Y.append(tuple(X))
-        M=Y
-    R=[process(gate,E) for E in X]
+        M = Y
+    return M
+
+
+def generate_input(gate, data):
+    if len(gate) < (1 << len(data)):
+        raise Exception("Too many inputs!")
+    M = generate_perms(data)
+    R = [process(gate, E) for E in M]
     return R
 
 
 class LogicGateNaiveGroup:
-    def __init__(self,size,safety_block=10):
-        if size>safety_block:
+    def __init__(self, size, safety_block=10):
+        if size > safety_block:
             raise Exception("Too big!")
-        self.size=size
-        X=[[]]
+        self.size = size
+        X = [[]]
         for i in range(self.size):
-            Y=[]
+            Y = []
             for E in X:
-                Y.extend([E+[0],E+[1]])
-        self.variants=[tuple(E) for E in X]
+                Y.extend([E + [0], E + [1]])
+        self.variants = [tuple(E) for E in X]
         return
 
 
 def main():
-    gate=(0,0,0,1)
-    res=process(gate,[0,1])
+    gate = (0, 0, 0, 1)
+    res = process(gate, [0, 1])
     for i in range(4):
-        print(process(gate,bin_to_tuple(i,2)))
+        print(process(gate, bin_to_tuple(i, 2)))
+    print(generate_perms([-1,1]))
+    print(generate_input(gate,[-1,1]))
     return
 
 
