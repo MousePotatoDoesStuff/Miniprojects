d006513414d851e7484d85005a8ef65015c98644
Dorijan Cirkveni <97680109+MousePotatoDoesStuff@users.noreply.github.com>
2023-08-01 23:37:13 +0200

    Create wfc_template.py

---
+++ wavefunction/wfc_template.py
@@ -0,0 +1,14 @@
+class WFCTemplate:
+    def
+
+
+class BaseWFC(WFCTemplate):
+    def __init__(self, variants, connections, ):
+
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()
