8d38a676089e173c7b9f4cbc9e91d3ba0fc98fb6
Dorijan Cirkveni <97680109+MousePotatoDoesStuff@users.noreply.github.com>
2023-08-07 22:23:05 +0200

    Create Bracket Combination.py

---
+++ coderbyte/Bracket Combination.py
@@ -0,0 +1,20 @@
+def BracketCombinationsNaive(num):
+  X=[(num,0)]
+  last=0
+  while len(X)>0:
+    Y=[]
+    for (a,b) in X:
+      if a>0:
+        E=(a-1,b+1)
+        Y.append(E)
+      if b>0:
+        E=(a,b-1)
+        Y.append(E)
+    last=len(X)
+    X=Y
+  return last
+
+
+if __name__ == "__main__":
+    for i in range(10):
+        print(BracketCombinationsNaive(i))
