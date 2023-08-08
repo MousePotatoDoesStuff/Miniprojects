fdb1f7c9380528ba960202deb592ee7784480738
Dorijan Cirkveni <97680109+MousePotatoDoesStuff@users.noreply.github.com>
2023-08-08 18:14:01 +0200

    done

--- coderbyte/MinWindowSubstring.py
+++ coderbyte/MinWindowSubstring.py
@@ -6,53 +6,50 @@ class charlog:
         for e in s:
             self.chars[e] -= 1
         return
+
     def __str__(self):
-        s=str(self.unsatisfied)
-        L=[e+"+-"[v<0]+str(abs(v)) for(e,v) in self.chars.items()]
-        return " [{}] ".format(s+": "+",".join(L))
+        s = str(self.unsatisfied)
+        L = [e + "+-"[v < 0] + str(abs(v)) for (e, v) in self.chars.items()]
+        return " [{}] ".format(s + ": " + ",".join(L))
 
     def add(self, s):
         for e in s:
             if e not in self.chars:
                 continue
-            print(str(self),end='+')
-            print(e,end='=')
             self.chars[e] += 1
             if self.chars[e] == 0:
                 self.unsatisfied -= 1
-            print(str(self))
         return
 
     def sub(self, s):
         for e in s:
-            print(str(self),end='-')
             if e not in self.chars:
-                self.chars[e] = 0
-            print(e,end='=')
+                return
             self.chars[e] -= 1
             if self.chars[e] == -1:
                 self.unsatisfied += 1
-            print(str(self))
         return
 
 
 def MinWindowSubstring(strArr):
-    L = strArr[2:][:-2].split('", "')
-    s = L[0]
+    s = strArr[0]
     n = len(s)
     start = 0
-    CL = charlog(L[1])
+    CL = charlog(strArr[1])
+    best = (n + 1, '')
     for end in range(0, n):
         CL.add(s[end])
-        while CL.unsatisfied == 0:
-            CL.sub(s[start])
-            start += 1
-        print(start,end+1,s[start:end+1])
-    return strArr
+        if CL.unsatisfied == 0:
+            while CL.unsatisfied == 0:
+                CL.sub(s[start])
+                start += 1
+            if best[0] > end + 2 - start:
+                best = end + 2 - start, s[start - 1:end + 1]
+    return best[1]
 
 
 def main():
-    print(MinWindowSubstring('["ahffaksfajeeubsne", "jefaa"]'))
+    print(MinWindowSubstring(["ab  c    ", "abc"]))
 
 
 if __name__ == "__main__":
