1e2ea2482e1110714f0d354455c83029ba3e1491
Dorijan Cirkveni <97680109+MousePotatoDoesStuff@users.noreply.github.com>
2023-08-08 13:30:50 +0200

    Update MinWindowSubstring.py

--- coderbyte/MinWindowSubstring.py
+++ coderbyte/MinWindowSubstring.py
@@ -6,23 +6,33 @@ class charlog:
         for e in s:
             self.chars[e] -= 1
         return
+    def __str__(self):
+        s=str(self.unsatisfied)
+        L=[e+"+-"[v<0]+str(abs(v)) for(e,v) in self.chars.items()]
+        return " [{}] ".format(s+": "+",".join(L))
 
     def add(self, s):
         for e in s:
             if e not in self.chars:
                 continue
+            print(str(self),end='+')
+            print(e,end='=')
             self.chars[e] += 1
             if self.chars[e] == 0:
                 self.unsatisfied -= 1
+            print(str(self))
         return
 
     def sub(self, s):
         for e in s:
+            print(str(self),end='-')
             if e not in self.chars:
                 self.chars[e] = 0
+            print(e,end='=')
             self.chars[e] -= 1
             if self.chars[e] == -1:
                 self.unsatisfied += 1
+            print(str(self))
         return
 
 
@@ -30,16 +40,14 @@ def MinWindowSubstring(strArr):
     L = strArr[2:][:-2].split('", "')
     s = L[0]
     n = len(s)
-    start=0
-    CL=charlog(L[1])
-    for end in range(0,n):
+    start = 0
+    CL = charlog(L[1])
+    for end in range(0, n):
         CL.add(s[end])
-        while CL.unsatisfied==0:
+        while CL.unsatisfied == 0:
             CL.sub(s[start])
-            print(s[start:end],end='->')
-            start+=1
-        print('|')
-
+            start += 1
+        print(start,end+1,s[start:end+1])
     return strArr
 
 
