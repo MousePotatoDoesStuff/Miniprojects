3e1e7b780c87aa6abaa13ed0d5bd0bbcd1f44b17
Dorijan Cirkveni <97680109+MousePotatoDoesStuff@users.noreply.github.com>
2023-08-08 13:14:07 +0200

    continuing

--- coderbyte/BracketCombination.py
+++ coderbyte/BracketCombination.py
@@ -1,28 +1,27 @@
 def BracketCombinationsNaive(num):
-  X=[(num,0)]
-  last=0
-  while len(X)>0:
-    Y=[]
-    for (a,b) in X:
-      if a>0:
-        E=(a-1,b+1)
-        Y.append(E)
-      if b>0:
-        E=(a,b-1)
-        Y.append(E)
-    last=len(X)
-    X=Y
-  return last
+    X = [(num, 0)]
+    last = 0
+    while len(X) > 0:
+        Y = []
+        for (a, b) in X:
+            if a > 0:
+                E = (a - 1, b + 1)
+                Y.append(E)
+            if b > 0:
+                E = (a, b - 1)
+                Y.append(E)
+        last = len(X)
+        X = Y
+    return last
+
 
 def BracketCombinations(num):
-    X=dict()
-    L=[(num,0)]
+    X = dict()
+    L = [(num, 0)]
     while True:
-        M=[]
+        M = []
         for E in L:
             pass
-
-
 
 
 if __name__ == "__main__":
--- coderbyte/MinWindowSubstring.py
+++ coderbyte/MinWindowSubstring.py
@@ -1,42 +1,50 @@
-def adjust(D,k,v,default=0):
-  e=D.get(k,default)+v
-  D[k]=e
-  return e
-def MinWindowSubstring(strArr):
-  s=strArr[0]
-  goal=strArr[1]
-  end=0
-  contents=dict()
-  for e in goal:
-    adjust(contents,e,-1)
-  unsatisfied=len(e.keys)
-  curbest=(len(s)+1,'')
-  for start in range(len(s)):
-    if unsatisfied!=0:
-      for curend in range(end,s):
-        c=s[curend]
-        if c not in contents:
-          continue
-        if adjust(contents,c,1)>=0:
-          unsatisfied-=1
-          if unsatisfied==0:
-            if curbest[0]> curend
-            end=curend
-            break
-      else:
-        break
-    if adjust(contents,s[start],-1)<0:
-      unsatisfied+=1
-  return strArr
+class charlog:
+    def __init__(self, s):
+        v = set(s)
+        self.chars = {e: 0 for e in v}
+        self.unsatisfied = len(v)
+        for e in s:
+            self.chars[e] -= 1
+        return
 
-# keep this function call here
-print(MinWindowSubstring(input()))
+    def add(self, s):
+        for e in s:
+            if e not in self.chars:
+                continue
+            self.chars[e] += 1
+            if self.chars[e] == 0:
+                self.unsatisfied -= 1
+        return
+
+    def sub(self, s):
+        for e in s:
+            if e not in self.chars:
+                self.chars[e] = 0
+            self.chars[e] -= 1
+            if self.chars[e] == -1:
+                self.unsatisfied += 1
+        return
+
+
+def MinWindowSubstring(strArr):
+    L = strArr[2:][:-2].split('", "')
+    s = L[0]
+    n = len(s)
+    start=0
+    CL=charlog(L[1])
+    for end in range(0,n):
+        CL.add(s[end])
+        while CL.unsatisfied==0:
+            CL.sub(s[start])
+            print(s[start:end],end='->')
+            start+=1
+        print('|')
+
+    return strArr
+
+
 def main():
-    for i in range(10):
-        print('???')
-    else:
-        print('!!!')
-    return
+    print(MinWindowSubstring('["ahffaksfajeeubsne", "jefaa"]'))
 
 
 if __name__ == "__main__":
