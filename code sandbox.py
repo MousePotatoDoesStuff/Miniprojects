f89d81e5191e2a111af360902c742eb650034cfb
Dorijan Cirkveni <97680109+MousePotatoDoesStuff@users.noreply.github.com>
2023-08-10 11:49:27 +0200

    Update code sandbox.py

--- code sandbox.py
+++ code sandbox.py
@@ -2,21 +2,18 @@ from typing import List
 
 
 class Solution:
-    def isValid(self, s: str) -> bool:
-        stack = []
-        pairs = {'(': ')', '[': ']', '{': '}'}
-        for e in s:
-            if e in pairs.keys():
-                stack.append(e)
-            else:
-                if len(stack) == 0 or pairs[stack.pop()] != e:
-                    return False
-        return len(stack) == 0
+    def maxProfit(self, prices: List[int]) -> int:
+        bestMinimum=prices[0]
+        bestProfit=0
+        for e in prices:
+            bestMinimum=min(bestMinimum,e)
+            f=e-bestMinimum
+            bestProfit=max(bestProfit,f)
+        return bestProfit
 
 
 def main():
     X = Solution()
-    print(X.isValid('()'))
     return
 
 
