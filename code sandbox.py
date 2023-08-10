f190bdf38bc1c612a4a41a17a2f07d3c24184d72
Dorijan Cirkveni <97680109+MousePotatoDoesStuff@users.noreply.github.com>
2023-08-10 12:52:46 +0200

    Update code sandbox.py

--- code sandbox.py
+++ code sandbox.py
@@ -2,42 +2,26 @@ from typing import List
 
 
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        start=0
-        n=len(nums)
-        if nums[0]>nums[-1]:
-            a=0
-            b=n
-            while b-a>1:
-                c=(a+b)//2
-                if nums[c]>nums[a]:
-                    a=c
-                else:
-                    b=c
-            start=b
-        shift=lambda x:(x+start)%n
-        a=0
-        b=n
-        while b-a>1:
-            c=(b+a)//2
-            d=nums[shift(c)]-target
-            if d==0:
-                return shift(c)
-            elif d>0:
-                b=c
-            else:
-                a=c
-        res=shift(a)
-        return res if nums[res]==target else -1
+    def myPow(self, x: float, n: int) -> float:
+        if n==0:
+            return 1
+        if n<0:
+            x=1/x
+            n=-n
+        cur=x
+        res=1
+        while n>0:
+            if n&1:
+                res*=cur
+            cur*=cur
+            n//=2
+        return res
+
 
 
 def main():
     X = Solution()
-    L=[4,5,6,7,0,1,2]
-    for e in L+[3]:
-        res=X.search(L,e)
-        print(res)
-    return
+    print(X.myPow(2,9))
 
 
 if __name__ == "__main__":
