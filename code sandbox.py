509eeffda433ea2b4a35e2fbf839dcc8515a1a0e
Dorijan Cirkveni <97680109+MousePotatoDoesStuff@users.noreply.github.com>
2023-08-10 10:49:26 +0200

    Update code sandbox.py

--- code sandbox.py
+++ code sandbox.py
@@ -1,9 +1,22 @@
-def combine(A:dict,B:dict):
-    C={**A,**B}
-    return
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        M=dict()
+        for i in range(len(nums)):
+            e=nums[i]
+            if e not in M:
+                M[e]=i
+            if target-e in M:
+                j=M[target-e]
+                if j!=i:
+                    return [j,i]
 
 
 def main():
+    X=Solution()
+    print(X.twoSum([3,3],6))
     return
 
 
