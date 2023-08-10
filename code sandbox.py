e42c92ae7a2ec25338000916a0c5e8176b82f481
Dorijan Cirkveni <97680109+MousePotatoDoesStuff@users.noreply.github.com>
2023-08-10 12:41:12 +0200

    Update code sandbox.py

--- code sandbox.py
+++ code sandbox.py
@@ -2,18 +2,41 @@ from typing import List
 
 
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        bestMinimum=prices[0]
-        bestProfit=0
-        for e in prices:
-            bestMinimum=min(bestMinimum,e)
-            f=e-bestMinimum
-            bestProfit=max(bestProfit,f)
-        return bestProfit
+    def search(self, nums: List[int], target: int) -> int:
+        start=0
+        n=len(nums)
+        if nums[0]>nums[-1]:
+            a=0
+            b=n
+            while b-a>1:
+                c=(a+b)//2
+                if nums[c]>nums[a]:
+                    a=c
+                else:
+                    b=c
+            start=b
+        shift=lambda x:(x+start)%n
+        a=0
+        b=n
+        while b-a>1:
+            c=(b+a)//2
+            d=nums[shift(c)]-target
+            if d==0:
+                return shift(c)
+            elif d>0:
+                b=c
+            else:
+                a=c
+        res=shift(a)
+        return res if nums[res]==target else -1
 
 
 def main():
     X = Solution()
+    L=[4,5,6,7,0,1,2]
+    for e in L+[3]:
+        res=X.search(L,e)
+        print(res)
     return
 
 
