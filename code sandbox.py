a5bbef402c0eb939b4d2f73fa6f3483fa6e5b450
Dorijan Cirkveni <97680109+MousePotatoDoesStuff@users.noreply.github.com>
2023-08-10 11:34:23 +0200

    Update code sandbox.py

--- code sandbox.py
+++ code sandbox.py
@@ -1,48 +1,22 @@
 from typing import List
 
 
-class Charset:
-    def __init__(self, s):
-        self.chars = dict()
-        self.repeating = None
-
-    def add_char(self, e):
-        re = False
-        x = 1 + self.chars.get(e, 0)
-        self.chars[e] = x
-        if x > 1:
-            self.repeating = e
-        return
-
-    def remove_chars(self, s):
-        for e in s:
-            x = -1 + self.chars.get(e, 0)
-            self.chars[e] = x
-        self.repeating = None
-        return
-
-
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        start = 0
-        CS = Charset('')
-        longest = 0
-        for end in range(len(s)):
-            e=s[end]
-            CS.add_char(e)
-            if CS.repeating == e:
-                longest = max(longest,end-start)
-                newstart = s[start:].index(e) + start + 1
-                CS.remove_chars(s[start:newstart])
-                start = newstart
-        longest=max(longest,len(s)-start)
-        return longest
+    def isValid(self, s: str) -> bool:
+        stack = []
+        pairs = {'(': ')', '[': ']', '{': '}'}
+        for e in s:
+            if e in pairs.keys():
+                stack.append(e)
+            else:
+                if len(stack) == 0 or pairs[stack.pop()] != e:
+                    return False
+        return len(stack) == 0
 
 
 def main():
     X = Solution()
-    print(X.lengthOfLongestSubstring('abcdefgh'))
-    print('wasdwasd'[2:].index('w') + 2)
+    print(X.isValid('()'))
     return
 
 
