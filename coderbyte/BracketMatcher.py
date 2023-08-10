7e99f0cf77836d6adf22985a95ecdd17cd116afc
Dorijan Cirkveni <97680109+MousePotatoDoesStuff@users.noreply.github.com>
2023-08-10 10:08:15 +0200

    Update BracketMatcher.py

--- coderbyte/BracketMatcher.py
+++ coderbyte/BracketMatcher.py
@@ -1,3 +1,15 @@
+def BracketMatcher(strParam):
+    x=0
+    for e in strParam:
+        if e=='(':
+            x+=1
+        elif e==')':
+            x-=1
+        if x<0:
+            return 0
+    return int(x==0)
+
+
 def main():
     return
 
