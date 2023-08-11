f456faac141b56969604335846719b8ff802dd1c
Dorijan Cirkveni <97680109+MousePotatoDoesStuff@users.noreply.github.com>
2023-08-11 17:51:28 +0200

    Update 30DaysOfPandas.py

--- leetcode/30DaysOfPandas.py
+++ leetcode/30DaysOfPandas.py
@@ -4,6 +4,9 @@ def big_countries(world: pd.DataFrame) -> pd.DataFrame:
     res=pd[pd['area']>3000000 and pd['population']>25000000]
     return res
 
+def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
+    return tweets[tweets["content"].str.len()>15][["tweet_id"]]
+
 def main():
     big_countries()
     return
