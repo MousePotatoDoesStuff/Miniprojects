027966a64c0b41e9dcde581196245dbc01890869
Dorijan Cirkveni <97680109+MousePotatoDoesStuff@users.noreply.github.com>
2023-08-11 21:28:12 +0200

    Update 30DaysOfPandas.py

--- leetcode/30DaysOfPandas.py
+++ leetcode/30DaysOfPandas.py
@@ -7,6 +7,9 @@ def big_countries(world: pd.DataFrame) -> pd.DataFrame:
 def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
     return tweets[tweets["content"].str.len()>15][["tweet_id"]]
 
+def valid_emails(users: pd.DataFrame) -> pd.DataFrame:
+    return users[users['mail'].str.match("[a-zA-Z][a-z0-9A-Z_.-]*@leetcode\.com")]
+
 def main():
     big_countries()
     return
