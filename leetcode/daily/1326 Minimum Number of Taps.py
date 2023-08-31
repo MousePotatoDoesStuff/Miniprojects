cab5ef96d8b7b829a6339f57a931e14edd9ba2bd
Dorijan Cirkveni <97680109+MousePotatoDoesStuff@users.noreply.github.com>
2023-08-31 10:57:36 +0200

    Update 1326 Minimum Number of Taps.py

--- leetcode/daily/1326 Minimum Number of Taps.py
+++ leetcode/daily/1326 Minimum Number of Taps.py
@@ -3,17 +3,19 @@ from typing import List
 
 class Solution:
     def minTaps(self, n: int, ranges: List[int]) -> int:
-        for (i,e) in enumerate(ranges):
-            ranges[i]=(i-e,i+e)
+        ranges=[(max(i-e,0),i+e) for (i,e) in enumerate(ranges) if e!=0]
         ranges.sort(key=lambda e:(e[0],-e[1]))
 
         j=0
-        for i in range(1,n+1):
+        print(ranges)
+        for i in range(1,len(ranges)):
             if ranges[i][0]!=ranges[j][0] and ranges[i][0]!=ranges[i][1]:
                 j+=1
                 ranges[j]=ranges[i]
-        for i in range(j,n):
+        print(ranges,j,n)
+        while len(ranges)>j+1:
             ranges.pop()
+        print(ranges,j,n)
         last_coverage=-1
         coverage=0
         j=0
@@ -26,14 +28,17 @@ class Solution:
                     last_coverage=coverage
                 ranges[j]=ranges[i]
                 coverage=ranges[i][1]
+        if coverage<n:
+            return -1
         for i in range(j,len(ranges)):
             ranges.pop()
-        return 0
+        return len(ranges)
 
 
 def main():
-    ranges=[0,1,2,1,0]
-    res=Solution().minTaps(4,ranges)
+    ranges=[3,4,1,1,0,0]
+    res=Solution().minTaps(5,ranges)
+    print(res)
     return
 
 
