39f9fa381f9938fe0000d5d375d06baf888cae4a
Dorijan Cirkveni <97680109+MousePotatoDoesStuff@users.noreply.github.com>
2023-08-31 11:07:44 +0200

    Update 1326 Minimum Number of Taps.py

--- leetcode/daily/1326 Minimum Number of Taps.py
+++ leetcode/daily/1326 Minimum Number of Taps.py
@@ -6,16 +6,13 @@ class Solution:
         ranges=[(max(i-e,0),min(i+e,n)) for (i,e) in enumerate(ranges) if e!=0]
         ranges.sort(key=lambda e:(e[0],-e[1]))
 
-        j=0
-        print(ranges)
+        L=[ranges[0]]
+        last=-1
         for i in range(1,len(ranges)):
-            if ranges[i][0]!=ranges[j][0] and ranges[i][0]!=ranges[i][1]:
-                j+=1
-                ranges[j]=ranges[i]
-        print(ranges,j,n)
-        while len(ranges)>j+1:
-            ranges.pop()
-        print(ranges,j,n)
+            if ranges[i][0]!=last:
+                last=ranges[i][0]
+                L.append(ranges[i])
+        ranges=L
         last_coverage=-1
         coverage=0
         count=0
