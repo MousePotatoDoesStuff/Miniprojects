b7c66c52f27311273bc89037f55f078525f90d5d
Dorijan Cirkveni <97680109+MousePotatoDoesStuff@users.noreply.github.com>
2023-09-01 21:03:53 +0200

    Update 1326 Minimum Number of Taps.py

--- leetcode/daily/1326 Minimum Number of Taps.py
+++ leetcode/daily/1326 Minimum Number of Taps.py
@@ -3,37 +3,37 @@ from typing import List
 
 class Solution:
     def minTaps(self, n: int, ranges: List[int]) -> int:
-        ranges=[(max(i-e,0),min(i+e,n)) for (i,e) in enumerate(ranges) if e!=0]
-        ranges.sort(key=lambda e:(e[0],-e[1]))
-        if len(ranges)==0:
+        ranges = [(max(i - e, 0), min(i + e, n)) for (i, e) in enumerate(ranges) if e != 0]
+        ranges.sort(key=lambda e: (e[0], -e[1]))
+        if len(ranges) == 0:
             return -1
 
-        L=[ranges[0]]
-        last=-1
-        for i in range(1,len(ranges)):
-            if ranges[i][0]!=last:
-                last=ranges[i][0]
+        L = [ranges[0]]
+        last = -1
+        for i in range(1, len(ranges)):
+            if ranges[i][0] != last:
+                last = ranges[i][0]
                 L.append(ranges[i])
-        ranges=L
-        last_coverage=-1
-        coverage=0
-        count=0
+        ranges = L
+        last_coverage = -1
+        coverage = 0
+        count = 0
         for i in range(len(ranges)):
-            if ranges[i][0]>coverage:
+            if ranges[i][0] > coverage:
                 return -1
-            if coverage<ranges[i][1]:
-                if last_coverage<ranges[i][0]:
-                    count+=1
-                    last_coverage=coverage
-                coverage=ranges[i][1]
-        if coverage<n:
+            if coverage < ranges[i][1]:
+                if last_coverage < ranges[i][0]:
+                    count += 1
+                    last_coverage = coverage
+                coverage = ranges[i][1]
+        if coverage < n:
             return -1
         return count
 
 
 def main():
-    ranges=[4,0,0,0,0,0,0,0,4]
-    res=Solution().minTaps(len(ranges)-1,ranges)
+    ranges = [4, 0, 0, 0, 0, 0, 0, 0, 4]
+    res = Solution().minTaps(len(ranges) - 1, ranges)
     print(res)
     return
 
