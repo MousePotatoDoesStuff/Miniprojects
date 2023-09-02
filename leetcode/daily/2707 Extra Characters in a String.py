2090116cc75baa5fb722a99f0f52ee91defde79a
Dorijan Cirkveni <97680109+MousePotatoDoesStuff@users.noreply.github.com>
2023-09-02 11:48:31 +0200

    Update 2707 Extra Characters in a String.py

--- leetcode/daily/2707 Extra Characters in a String.py
+++ leetcode/daily/2707 Extra Characters in a String.py
@@ -2,8 +2,9 @@ from typing import List
 
 
 class State:
-    def __init__(self, count=0, last=0):
+    def __init__(self, count=0, word=-1, last=0):
         self.count = count
+        self.word = word
         self.last = last
         return
 
@@ -13,39 +14,37 @@ class State:
         b = other.count - other.last
         return int(a > b) - int(a < b)
 
-
     def __str__(self):
-        return "{}->{}".format(self.last,self.count)
+        return "{}:{}->{}".format(self.word, self.last, self.count)
 
 
 class Solution:
     def __init__(self):
         self.words = []
         self.begins = dict()
-        self.states = dict()
+        self.null = State()
+        self.states: List[State] = []
         return
 
     def step(self, ind, char):
-        blank: State = self.states[-1]
-        for word in set(self.states.keys()) - {-1}:
-            state = self.states[word]
+        new_states = []
+        while len(self.states) != 0:
+            state = self.states.pop()
+            word = state.word
             diff = ind - state.last
             if len(self.words[word]) == diff:
-                state.last=ind
-                if blank.comp(state) > 0:
-                    blank = state
-                self.states.pop(word)
+                if diff==1:
+                    print('???')
+                state.last = ind
+                if self.null.comp(state) > 0:
+                    self.null = state
                 continue
-            if char != self.words[word][diff]:
-                self.states.pop(word)
-                continue
-        a = blank.count + ind - blank.last
+            if char == self.words[word][diff]:
+                new_states.append(state)
+        states=new_states
+        a = self.null.count + ind - self.null.last
         for word in self.begins.get(char, []):
-            self.states[word] = State(a,ind)
-        self.states[-1]=blank
-        print(ind,"|----|")
-        for e in self.states:
-            print(e,self.states[e])
+            self.states.append(State(a, word, ind))
         return
 
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
@@ -54,16 +53,19 @@ class Solution:
         self.begins = {e[0]: set() for e in self.words}
         for i, e in enumerate(dictionary):
             self.begins[e[0]].add(i)
-        self.states[-1] = State()
+        self.null = State()
         for (i, e) in enumerate(s):
             self.step(i, e)
-        return self.states[-1].count+i-self.states[-1].last
+        print(self.null.count,len(s) - 1,self.null.last)
+        return self.null.count + len(s) - 1 - self.null.last
 
 
 def main():
-    s="leetscode"
-    L=["leet","code","leetcode"]
-    res=Solution().minExtraChar(s,L)
+    s = "voctv ochpg  utoyw pnafy  lzelq snzsb  andjc qdciy  oefi".replace(' ','')
+    print(len(s))
+    L = ["tf", "v", "wadrya", "a", "cqdci", "uqfg", "voc", "zelqsn", "band", "b", "yoefi", "utoywp", "herqqn", "umra",
+         "frfuyj", "vczatj", "sdww"]
+    res = Solution().minExtraChar(s, L)
     print(res)
     return
 
