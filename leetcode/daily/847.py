26d69be262fbb25634fa4956aa4519816337e366
Dorijan Cirkveni <97680109+MousePotatoDoesStuff@users.noreply.github.com>
2023-09-18 08:51:21 +0200

    leetcode

---
+++ leetcode/daily/847.py
@@ -0,0 +1,16 @@
+class
+class Solution(object):
+    def shortestPathLength(self, graph):
+        """
+        :type graph: List[List[int]]
+        :rtype: int
+        """
+        X=[{(i,)}for i in range(len(graph))]
+
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()
