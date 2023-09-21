9e9974a27bf6c35bad5a4cf38c5cdca95efc4872
Dorijan Cirkveni <97680109+MousePotatoDoesStuff@users.noreply.github.com>
2023-09-21 10:33:20 +0200

    median

---
+++ leetcode/special/4.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def __init__(self):
+        self.extras = []
+
+    def findMedian(self,L,delta=0,get_indices=False):
+        n=len(L)+abs(delta)
+        half=(n-1)//2
+        if delta<0:
+            half-=delta
+        other=half if n&1 else half+1
+        if get_indices:
+            self.extras.append((self,other))
+        return L[half] if half==other else (L[half]+L[other])/2
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+
+
+def main():
+    sol=Solution()
+    print(sol.findMedian([1,4,5,7,22]))
+    print(sol.findMedian([1,4,5,7,22,69,420,1337]))
+    return
+
+
+if __name__ == "__main__":
+    main()
