0546e3e3726e55f4ad4d7cfcb36eef12e7ddc141
Dorijan Cirkveni <97680109+MousePotatoDoesStuff@users.noreply.github.com>
2023-09-21 12:18:16 +0200

    Update 4.py

--- leetcode/special/4.py
+++ leetcode/special/4.py
@@ -1,18 +1,48 @@
+from bisect import bisect
+
+
+def EndList(n,l):
+    return n if n>=0 else len()
+
+
 class Solution(object):
     def __init__(self):
         self.extras = []
 
-    def findMedian(self,L,delta=0,get_indices=False):
-        n=len(L)+abs(delta)
+    def findMedian(self,L,delta=0,start=0,end=-1,get_indices=False):
+        end=EndList(end,L)
+        n=end-start+abs(delta)
         half=(n-1)//2
+        half+=start
         if delta<0:
             half+=delta
         other=half if n&1 else half+1
         if get_indices:
             self.extras.append((self,other))
         return L[half] if half==other else (L[half]+L[other])/2
-    def findMedianOverlap(self, nums1, nums2):
-
+    def findGroupStartOverlap(self, nums1, nums2, start1=0, start2=0, end1=-1, end2=-1):
+        end1=EndList(end1,nums1)
+        end2=EndList(end2,nums2)
+        A1=nums1[start1]
+        A2=nums2[start2]
+        swc=False
+        if A1>A2:
+            swc=True
+            nums1,nums2=nums2,nums1
+            start1,start2=start2,start1
+            end1,end2=end2,end1
+            A1,A2=A2,A1
+        B1=nums1[end1]
+        B2=nums2[end2]
+        if B1<=A2:
+            return None,swc
+        res1 = start1
+        res2 = start2
+        if A1<=A2:
+            res1=bisect(nums1,A2)
+        if A2>=A1:
+            res2=bisect(nums2,A1)
+        return (res2,res1) if swc else (res1,res2)
     def findMedianSortedArrays(self, nums1, nums2):
         """
         :type nums1: List[int]
@@ -23,9 +53,9 @@ class Solution(object):
 
 def main():
     sol=Solution()
-    d=-2
-    print(sol.findMedian([1,4,5,7,22],d))
-    print(sol.findMedian([1,4,5,7,22,69,420,1337],d))
+    d=3
+    print(sol.findMedian([1,4,5,7,22],d,1,4))
+    print(sol.findMedian([1,4,5,7,22,69,420,1337],d,1,7))
     return
 
 
