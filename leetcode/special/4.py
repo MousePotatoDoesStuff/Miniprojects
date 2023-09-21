b93f29da1c465cb0f680a47ab040e6eb6b00311f
Dorijan Cirkveni <97680109+MousePotatoDoesStuff@users.noreply.github.com>
2023-09-21 17:05:39 +0200

    Update 4.py

--- leetcode/special/4.py
+++ leetcode/special/4.py
@@ -61,9 +61,9 @@ class Solution(object):
         return SwapIf(start_overlap, swc), SwapIf(end_overlap, swc)
 
     def medianSplit(self, nums1, nums2, start1, start2, end1, end2, delta=0):
-        self.findMedian(nums1,delta,start1,end1,True)
+        A1,B1=self.findMedian(nums1,delta,start1,end1,True)
         med_a1,med_b1=self.extras.pop()
-        self.findMedian(nums2,delta,start2,end2,True)
+        A2,B2=self.findMedian(nums2,delta,start2,end2,True)
         med_a2,med_b2=self.extras.pop()
         diff=nums2[med_a2]-nums1[med_a1]
         if diff>=0:
@@ -76,6 +76,33 @@ class Solution(object):
         if diff>=0:
             end2=med_b2+1
         return (start1,end1),(start2,end2)
+    def medianFinisher(self, nums1, nums2, start1, start2, end1, end2, delta=0):
+        n=len(nums1)+len(nums2)
+        if delta<0:
+            delta*=-1
+            n+=delta
+            while delta<n-delta:
+                if start1==end1:
+                    return self.findMedian(nums2,delta,start2,end2)
+                if start2==end2:
+                    return self.findMedian(nums1,delta,start1,end1)
+                if nums1[start1-1]<nums2[start2-1]:
+                    start2-=1
+                else:
+                    start1-=1
+                delta+=1
+        else:
+            n+=delta
+            while delta<n-delta:
+                if start1==end1:
+                    return self.findMedian(nums2,delta,start2,end2)
+                if start2==end2:
+                    return self.findMedian(nums1,delta,start1,end1)
+                if nums1[end1-1]<nums2[end2-1]:
+                    end2-=1
+                else:
+                    end1-=1
+                delta+=1
 
     def findMedianSortedArrays(self, nums1, nums2):
         """
@@ -97,9 +124,9 @@ class Solution(object):
 
 def main():
     sol = Solution()
-    A = [-1] * 20 + [1, 2, 3, 4, 5]
+    A = [1, 2, 3, 4, 5]
     B = [3, 4, 5, 6, 7]
-    res = sol.findGroupOverlap(A, B)
+    res = sol.medianFinisher(A,B,0,0,5,5,0)
     print(res)
     return
 
