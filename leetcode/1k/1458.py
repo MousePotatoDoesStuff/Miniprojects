223bbb138be9ee84687dae22fd537fd743826f71
Dorijan Cirkveni <97680109+MousePotatoDoesStuff@users.noreply.github.com>
2023-10-08 12:43:00 +0200

    Update 1458.py

--- leetcode/1k/1458.py
+++ leetcode/1k/1458.py
@@ -16,57 +16,54 @@ class Solution:
                 while prevs[pr_ind][0] < j:
                     nexts.append(prevs[pr_ind])
                     pr_ind += 1
-                E = (j, prevs[pr_ind-1][1] + e * f)
+                add = e * f
+                if add <= 0:
+                    continue
+                E = (j, prevs[pr_ind - 1][1] + add)
                 if prevs[pr_ind][0] == j:
                     E = max(E, prevs[pr_ind])
                     pr_ind += 1
                 nexts.append(E)
+            prevs.pop()
+            for j in range(pr_ind,len(prevs)):
+                nexts.append(prevs[j])
             prevs = []
             last = -1
             for e in nexts:
-                if e[1]>last:
+                if e[1] > last:
                     prevs.append(e)
-                    last=e[1]
-            prevs.pop()
+                    last = e[1]
         return prevs[-1][1]
 
     def filter(self, nums):
-        plus = []
-        minus = []
+        other = []
         zero = False
         for e in nums:
-            if e > 0:
-                plus.append(e)
+            if e == 0:
+                zero = True
                 continue
-            if e < 0:
-                minus.append(-e)
-                continue
-            zero = True
-        return plus, minus, zero
+            other.append(e)
+        return other, zero
 
     def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
-        plus1, minus1, zero1 = self.filter(nums1)
-        plus2, minus2, zero2 = self.filter(nums2)
-        if not ((plus1 and plus2) or (minus1 and minus2)):
-            best = -(10**9+7)
+        nums1, zero1 = self.filter(nums1)
+        nums2, zero2 = self.filter(nums2)
+        a1, b1 = min(nums1),max(nums1)
+        a2, b2 = min(nums2), max(nums2)
+        if b1 < 0 < a2:
             if zero1 or zero2:
                 return 0
-            if plus1 and minus2:
-                best = max(best, -min(plus1) * min(minus2))
-            if plus2 and minus1:
-                best = max(best, -min(plus2) * min(minus1))
-            return best
-        best=0
-        if plus1 and plus2:
-            best+=self.maxDotProductPositive(plus1,plus2)
-        if minus1 and minus2:
-            best+=self.maxDotProductPositive(minus1,minus2)
-        return best
+            return b1*a2
+        if b2 < 0 < a1:
+            if zero1 or zero2:
+                return 0
+            return b1*a2
+        return self.maxDotProductPositive(nums1,nums2)
 
 
 def main():
-    SOL=Solution()
-    res=SOL.maxDotProduct([2,1,-2,5],[3,0,-6])
+    SOL = Solution()
+    res = SOL.maxDotProduct([3,-2], [2,-6,7])
     print(res)
     return
 
