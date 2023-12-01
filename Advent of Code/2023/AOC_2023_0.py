98878c67a6e41d60d072068caebefe92bfe3b0f9
Dorijan Cirkveni <97680109+MousePotatoDoesStuff@users.noreply.github.com>
2023-12-01 21:42:00 +0100

    mayhaps

--- Advent of Code/2023/AOC_2023_0.py
+++ Advent of Code/2023/AOC_2023_0.py
@@ -1,11 +1,18 @@
+from pathlib import Path
+
+
 def solve(IN):
     return IN
 
 
-IN = "TEST"
+in_loc = "AOC_2023_{}.txt"
 
 
 def main():
+    num=Path(__file__).name.split('_')[-1][:-3]
+    F=open(in_loc.format(num),'r')
+    IN=F.read()
+    F.close()
     OUT = solve(IN)
     print(OUT)
     return
