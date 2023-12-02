1fc86d12cf22a2bb0a037d23b2b1fee65a820ce6
Dorijan Cirkveni <97680109+MousePotatoDoesStuff@users.noreply.github.com>
2023-12-02 22:41:29 +0100

    AOC 2 complete

--- Advent of Code/2023/AOC_2023_2.py
+++ Advent of Code/2023/AOC_2023_2.py
@@ -10,10 +10,10 @@ def decipher(gameRaw):
         temp={e:0 for e in 'rgb'}
         for f in F:
             G=f.split(' ')
-            temp[G[1][0]]+=int()
+            temp[G[1][0]]+=int(G[0])
         for e,v in temp.items():
             res[e]=max(res[e],v)
-    return res
+    return gameID,res
 def checkIfFits(game:dict,ref:dict):
     for e,v in game.items():
         if v>ref.get(e,0):
@@ -23,8 +23,20 @@ def checkIfFits(game:dict,ref:dict):
 
 def solve(IN):
     L=IN.split("\n")
-    print(decipher(L[0]))
-    return IN
+    ref={'r':12,'g':13,'b':14}
+    res=0
+    mode=data.get('version',0)
+    for gameRaw in L:
+        gameID,game=decipher(gameRaw)
+        if mode==1:
+            if checkIfFits(game,ref):
+                res+=gameID
+        else:
+            temp=1
+            for e in game.values():
+                temp*=e
+            res+=temp
+    return res
 
 
 in_loc = "AOC_2023_{}.txt"
