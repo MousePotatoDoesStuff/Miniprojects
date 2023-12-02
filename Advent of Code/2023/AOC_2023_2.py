43dfe6602659ca8f9273e840a7b25ef3aeb18ba4
Dorijan Cirkveni <97680109+MousePotatoDoesStuff@users.noreply.github.com>
2023-12-02 22:35:02 +0100

    AOC 2

--- Advent of Code/2023/AOC_2023_2.py
+++ Advent of Code/2023/AOC_2023_2.py
@@ -1,7 +1,29 @@
 from pathlib import Path
 
+def decipher(gameRaw):
+    game=gameRaw.split(": ")
+    gameID=int(game[0][5:])
+    game=game[1].split("; ")
+    res={e:0 for e in 'rgb'}
+    for E in game:
+        F=E.split(', ')
+        temp={e:0 for e in 'rgb'}
+        for f in F:
+            G=f.split(' ')
+            temp[G[1][0]]+=int()
+        for e,v in temp.items():
+            res[e]=max(res[e],v)
+    return res
+def checkIfFits(game:dict,ref:dict):
+    for e,v in game.items():
+        if v>ref.get(e,0):
+            return False
+    return True
+
 
 def solve(IN):
+    L=IN.split("\n")
+    print(decipher(L[0]))
     return IN
 
 
