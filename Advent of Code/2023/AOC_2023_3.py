d70ca44db2f4d0fea366791fad34a19b44244eb5
Dorijan Cirkveni <97680109+MousePotatoDoesStuff@users.noreply.github.com>
2023-12-03 10:43:38 +0100

    Update AOC_2023_3.py

--- Advent of Code/2023/AOC_2023_3.py
+++ Advent of Code/2023/AOC_2023_3.py
@@ -1,29 +1,58 @@
+from collections import deque
 from pathlib import Path
 
 
+def identify(line, notit):
+    return [i for i, e in enumerate(line) if e not in notit] + [10000]
+
+
 def solve(IN):
-    lim="."*len(IN[0])
-    IN=[lim]+['.'+e+'.' for e in IN]+[lim]
-    n=len(IN)
-    m=len(IN[0])
-    notit=set("0123456789.")
-    neigh=set()
-    for i in range(-1,2):
-        for j in range(-1,2):
-            neigh.add((i,j))
-    neigh.remove((0,0))
-    used=set()
-    for i,E in enumerate(IN):
-        for j,e in enumerate(E):
-            if e not in notit:
-                for k,k2 in neigh:
-                    used.add((i+k,j+k2))
+    IN = IN.split("\n")
+    m = len(IN[0])
+    lim = "." * m
+    IN = [lim] + ['.' + e + '.' for e in IN] + [lim]
+    notit = set("0123456789.")
+    Q = deque()
+    Q.append([1000])
+    Q.append([1000])
+    Q.append([1000])
+    res = 0
+    adj = []
+    for E in IN:
+        print(E)
+        Q.append(identify(E, notit))
+        Q.popleft()
+        curadj = ""
+        curind = [0] * 3
+        for i in range(m):
+            check = False
+            for j in range(3):
+                e = curind[j]
+                while Q[j][e] < i - 1:
+                    e += 1
+                if Q[j][e] < i + 2:
+                    check = True
+                curind[j] = e
+            if check:
+                curadj += "+"
+            else:
+                curadj += " "
+        adj.append(curadj)
     notit.remove('.')
     res=0
-    for e in used:
-        E=IN[e[0]][e[1]]
-        if E in notit:
-            res+=int(E)
+    for i,E in enumerate(IN):
+        curnum=0
+        ack=False
+        for j,e in enumerate(E):
+            if e in notit:
+                curnum*=10
+                curnum+=int(e)
+                if not ack and adj[i][j]:
+                    ack=True
+            elif curnum!=0:
+                if ack:
+                    res+=curnum
+                curnum=0
     return res
 
 
@@ -32,9 +61,9 @@ data = {'version': 2}
 
 
 def main():
-    num=Path(__file__).name.split('_')[-1][:-3]
-    F=open(in_loc.format(num),'r')
-    IN=F.read()
+    num = Path(__file__).name.split('_')[-1][:-3]
+    F = open(in_loc.format(num), 'r')
+    IN = F.read()
     F.close()
     OUT = solve(IN)
     print(OUT)
