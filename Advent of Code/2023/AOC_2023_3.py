a3b5f4429c31840ae64a09aeba89d668f0921733
Dorijan Cirkveni <97680109+MousePotatoDoesStuff@users.noreply.github.com>
2023-12-03 13:52:06 +0100

    Update AOC_2023_3.py

--- Advent of Code/2023/AOC_2023_3.py
+++ Advent of Code/2023/AOC_2023_3.py
@@ -2,25 +2,31 @@ from collections import deque
 from pathlib import Path
 
 
-def identify(line, notit):
-    return [i for i, e in enumerate(line) if e not in notit] + [10000]
+def identify(line, notit, swc):
+    key = lambda e: e == '*'
+    if swc:
+        key = lambda e: e not in notit
+    return [i for i, e in enumerate(line) if key(e)] + [10000]
 
 
 def solve(IN):
-    IN = IN.split("\n")
-    m = len(IN[0])
-    lim = "." * m
-    IN = [lim] + ['.' + e + '.' for e in IN] + [lim]
+    swc = data.get('version') == 1
+    print(swc)
     notit = set("0123456789.")
+    print(set(IN) - notit)
+    IN = IN.split("\n")
+    m = len(IN[0]) + 2
+    lim = "." * (m)
+    IN = [lim] + ['.' + e + '.' for e in IN] + [lim]
     Q = deque()
     Q.append([1000])
     Q.append([1000])
     Q.append([1000])
     res = 0
     adj = []
+    skip = None
     for E in IN:
-        print(E)
-        Q.append(identify(E, notit))
+        Q.append(identify(E, notit,swc))
         Q.popleft()
         curadj = ""
         curind = [0] * 3
@@ -37,24 +43,109 @@ def solve(IN):
                 curadj += "+"
             else:
                 curadj += " "
+        if skip is None:
+            skip = curadj
+            continue
         adj.append(curadj)
+    adj.append(skip)
+    print(len(IN[0]), len(adj[0]))
     notit.remove('.')
-    res=0
-    for i,E in enumerate(IN):
-        curnum=0
-        ack=False
-        for j,e in enumerate(E):
+    res = 0
+    check = []
+    for i, E in enumerate(IN):
+        chl = ''
+        chlt = ''
+        curnum = 0
+        ack = False
+        for j, e in enumerate(E):
             if e in notit:
-                curnum*=10
-                curnum+=int(e)
-                if not ack and adj[i][j]:
-                    ack=True
-            elif curnum!=0:
+                curnum *= 10
+                curnum += int(e)
+                if not ack and adj[i][j] == "+":
+                    ack = True
+                chlt += e
+                continue
+            if curnum != 0:
                 if ack:
-                    res+=curnum
-                curnum=0
+                    res += curnum
+                    chl += chlt
+                    ack = False
+                else:
+                    chl += '.' * len(chlt)
+                chlt = ''
+                curnum = 0
+            chl += ['*X', ' +'][e == '.'][adj[i][j] == '+']
+        check.append(chl)
+    print("\n".join(check))
     return res
 
+def solve2(IN):
+    notit = set("0123456789.")
+    print(set(IN) - notit)
+    IN = IN.split("\n")
+    m = len(IN[0]) + 2
+    lim = "." * m
+    IN = [lim] + ['.' + e + '.' for e in IN] + [lim]
+    Q = deque()
+    Q.append([1000])
+    Q.append([1000])
+    Q.append([1000])
+    res = 0
+    adj = []
+    skip = None
+    for E in IN:
+        Q.append(identify(E, notit,False))
+        Q.popleft()
+        curadj = ""
+        curind = [0] * 3
+        for i in range(m):
+            check = False
+            for j in range(3):
+                e = curind[j]
+                while Q[j][e] < i - 1:
+                    e += 1
+                if Q[j][e] < i + 2:
+                    check = True
+                curind[j] = e
+            if check:
+                curadj += "+"
+            else:
+                curadj += " "
+        if skip is None:
+            skip = curadj
+            continue
+        adj.append(curadj)
+    adj.append(skip)
+    print(len(IN[0]), len(adj[0]))
+    notit.remove('.')
+    res = 0
+    check = []
+    for i, E in enumerate(IN):
+        chl = ''
+        chlt = ''
+        curnum = 0
+        ack = False
+        for j, e in enumerate(E):
+            if e in notit:
+                curnum *= 10
+                curnum += int(e)
+                if not ack and adj[i][j] == "+":
+                    ack = True
+                chlt += e
+                continue
+            if curnum != 0:
+                if ack:
+                    res += curnum
+                    chl += chlt
+                    ack = False
+                else:
+                    chl += '.' * len(chlt)
+                chlt = ''
+                curnum = 0
+            chl += ['*X', ' +'][e == '.'][adj[i][j] == '+']
+        check.append(chl)
+    print("\n".join(check))
+    return res
 
 in_loc = "AOC_2023_{}.txt"
 data = {'version': 2}
