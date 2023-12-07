ee86d2a8d468fa53c6487b7f4a59f1e96a14f9a5
Dorijan Cirkveni <97680109+MousePotatoDoesStuff@users.noreply.github.com>
2023-12-07 17:10:20 +0100

    new

--- Advent of Code/2023/AOC_2023_4.py
+++ Advent of Code/2023/AOC_2023_4.py
@@ -1,11 +1,35 @@
 from pathlib import Path
 
 
+def decipher_card(raw):
+    L=raw.split('\n')
+    RES=[]
+    for Eraw in L:
+        E=" "+Eraw[10:].replace("  "," ")
+        E=E[1:].split(" | ")
+        E=[[int(e) for e in F.split(" ") if e] for F in E]
+        RES.append(E)
+    return RES
+
+
 def solve1(IN):
-    return "TEST"
+    TEMP=decipher_card(IN)
+    res=0
+    for (A,B) in TEMP:
+        C=set(A)&set(B)
+        print(C,end="")
+        rest=len(C)
+        if rest!=0:
+            res+=1<<(rest-1)
+    print()
+    return res
 
 def solve2(IN):
-    return "MIKE CHECK"
+    TEMP=decipher_card(IN)
+    res=0
+    WINNERS=[len(set(A)&set(B)) for (A,B) in TEMP]
+    print(WINNERS[-20:])
+    return res
 
 in_loc = "AOC_2023_{}.txt"
 data = {'version': 2}
