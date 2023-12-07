c242eedd0d17ed37e6d0b0d665835c77f85ac398
Dorijan Cirkveni <97680109+MousePotatoDoesStuff@users.noreply.github.com>
2023-12-07 16:52:32 +0100

    Update AOC_2023_3.py

--- Advent of Code/2023/AOC_2023_3.py
+++ Advent of Code/2023/AOC_2023_3.py
@@ -16,17 +16,16 @@ def solve(IN):
     print(set(IN) - notit)
     IN = IN.split("\n")
     m = len(IN[0]) + 2
-    lim = "." * (m)
+    lim = "." * m
     IN = [lim] + ['.' + e + '.' for e in IN] + [lim]
     Q = deque()
     Q.append([1000])
     Q.append([1000])
     Q.append([1000])
-    res = 0
     adj = []
     skip = None
     for E in IN:
-        Q.append(identify(E, notit,swc))
+        Q.append(identify(E, notit, swc))
         Q.popleft()
         curadj = ""
         curind = [0] * 3
@@ -79,73 +78,59 @@ def solve(IN):
     print("\n".join(check))
     return res
 
+
 def solve2(IN):
-    notit = set("0123456789.")
-    print(set(IN) - notit)
     IN = IN.split("\n")
     m = len(IN[0]) + 2
     lim = "." * m
     IN = [lim] + ['.' + e + '.' for e in IN] + [lim]
-    Q = deque()
-    Q.append([1000])
-    Q.append([1000])
-    Q.append([1000])
-    res = 0
-    adj = []
-    skip = None
-    for E in IN:
-        Q.append(identify(E, notit,False))
-        Q.popleft()
-        curadj = ""
-        curind = [0] * 3
-        for i in range(m):
-            check = False
-            for j in range(3):
-                e = curind[j]
-                while Q[j][e] < i - 1:
-                    e += 1
-                if Q[j][e] < i + 2:
-                    check = True
-                curind[j] = e
-            if check:
-                curadj += "+"
-            else:
-                curadj += " "
-        if skip is None:
-            skip = curadj
-            continue
-        adj.append(curadj)
-    adj.append(skip)
-    print(len(IN[0]), len(adj[0]))
-    notit.remove('.')
-    res = 0
-    check = []
+    adj = dict()
+    resnums = dict()
+    count = dict()
     for i, E in enumerate(IN):
-        chl = ''
-        chlt = ''
+        for j, F in enumerate(E):
+            if F == "*":
+                S1 = (i, j)
+                resnums[S1] = 1
+                count[S1]=0
+                for k1 in range(-1, 2):
+                    for k2 in range(-1, 2):
+                        S2 = (i + k1, j + k2)
+                        TS = adj.get(S2, set())
+                        TS.add(S1)
+                        adj[S2] = TS
+    print(adj)
+    notit = set("0123456789")
+    for i, E in enumerate(IN):
         curnum = 0
-        ack = False
+        ack = set()
         for j, e in enumerate(E):
             if e in notit:
                 curnum *= 10
                 curnum += int(e)
-                if not ack and adj[i][j] == "+":
-                    ack = True
-                chlt += e
+                if (i, j) in adj:
+                    ack |= adj[(i, j)]
                 continue
             if curnum != 0:
                 if ack:
-                    res += curnum
-                    chl += chlt
-                    ack = False
-                else:
-                    chl += '.' * len(chlt)
-                chlt = ''
+                    for E in ack:
+                        resnums[E] *= curnum
+                        count[E]+=1
+                    ack = set()
                 curnum = 0
-            chl += ['*X', ' +'][e == '.'][adj[i][j] == '+']
-        check.append(chl)
-    print("\n".join(check))
-    return res
+    D=dict()
+    for e in count.values():
+        D[e]=D.get(e,0)+1
+    res=0
+    res2=0
+    for e in count:
+        res2+=resnums[e]
+        if count[e]<2:
+            print(count[e],e,resnums[e])
+            continue
+        res+=resnums[e]
+    return res,res2
+
 
 in_loc = "AOC_2023_{}.txt"
 data = {'version': 2}
@@ -156,7 +141,7 @@ def main():
     F = open(in_loc.format(num), 'r')
     IN = F.read()
     F.close()
-    OUT = solve(IN)
+    OUT = solve2(IN)
     print(OUT)
     return
 
