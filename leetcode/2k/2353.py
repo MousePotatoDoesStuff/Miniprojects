442dc192e3788d15e484a09deefc85120421579e
Dorijan Cirkveni <97680109+MousePotatoDoesStuff@users.noreply.github.com>
2023-12-17 10:40:04 +0100

    Update 2353.py

--- leetcode/2k/2353.py
+++ leetcode/2k/2353.py
@@ -1,29 +1,39 @@
+import heapq
+from typing import List
+
+
 class FoodRatings:
 
     def __init__(self, foods: List[str], cuisines: List[str], ratings: List[int]):
-        self.byRating = dict()
-        self.byFood = dict()
+        self.byRating: dict = dict()
+        self.byFood: dict = dict()
         self.ratings = []
 
     def clearRatings(self):
         while not self.byRating[self.ratings[0]]:
-            e = heapq.heappop(self.ratings)
-            self.byRating.
+            e = -(heapq.heappop(self.ratings))
+            self.byRating.pop(e)
         return
 
     def addRating(self, food, rating):
         if rating in self.byRating:
             S = self.byRating[rating]
-            heapq.heappush(S, food)
+            S.add(food)
         else:
-            self.byRating[rating] = [food]
-            heapq.heappush(self.ratings, rating)
+            self.byRating[rating] = {food}
+            heapq.heappush(self.ratings, -rating)
         self.byFood[food] = rating
+        return
 
     def changeRating(self, food: str, newRating: int) -> None:
-        oldRating = self.byFood[rating]
+        oldRating = self.byFood[food]
+        self.byFood[food] = newRating
+        self.byRating[oldRating] -= {food}
+        self.addRating(food, newRating)
+        return
 
     def highestRated(self, cuisine: str) -> str:
+        return min(self.byRating[self.ratings[0]])
 
 
 # Your FoodRatings object will be instantiated and called as such:
