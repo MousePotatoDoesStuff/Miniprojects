7853e8d9013306cd2b68d0b011845146ecdac342
Dorijan Cirkveni <mousepotato@tutanota.com>
2024-11-09 20:41:44 +0100

    Update 10tips.py

--- 10tips.py
+++ 10tips.py
@@ -1,5 +1,6 @@
 # https://levelup.gitconnected.com/10-ways-to-write-better-python-codes-55fc862ab0ef
 from collections import Counter, deque
+from dataclasses import dataclass
 
 
 def generatorExample(test: list[int]):
@@ -88,7 +89,6 @@ def memo_collatz(n: int, store: dict[int, int]):
     return store[n]
 
 
-
 def tip5():
     """
     Use memos as a space-for-time tradeoff.
@@ -96,8 +96,83 @@ def tip5():
     """
     return
 
+def wrapper_fn(func):
+    def wrapper():
+        print("Starting function...")
+        func()
+        print("Ending function...")
+        return
+    return wrapper
+
+@wrapper_fn
+def wrapped_fn():
+    print("Hello World")
+    return
+
+
+def tip6():
+    """
+    Wrap functions.
+    :return:
+    """
+    wrapped_fn()
+    return
+
+@dataclass
+class Clone:
+    name: str
+    iteration: int
+    def next(self):
+        return Clone(self.name,self.iteration+1)
+
+
+def tip7():
+    """
+    Use dataclasses.
+    :return:
+    """
+    X=Clone('Joe',1)
+    Y=X.next()
+    print(X,Y)
+    return
+
+
+def tip8():
+    """
+    Use match case (PYTHON 3.10 AND UP!)
+    :return:
+    """
+    return
+
+
+def tip9():
+    """
+    all and any operators
+    :return:
+    """
+    A=[0,1,2]
+    print(all(e>0 for e in A))
+    print(all(e<3 for e in A))
+    print(any(e>1 for e in A))
+    print(any(e<-2 for e in A))
+    return
+
+
+def tip10():
+    """
+    comprehensions
+    :return:
+    """
+    L=[0,1,2,1,0]
+    T=tuple(e for e in L)
+    S={e for e in T}
+    D={e:L.index(e) for e in S}
+    print(D)
+    return
+
+
 def main():
-    tip4()
+    tip6()
     return
 
 
