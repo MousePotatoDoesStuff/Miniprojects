20ad5614b797197b23807417af6e95ab8e80c3d1
Dorijan Cirkveni <mousepotato@tutanota.com>
2024-11-29 19:01:01 +0100

    Update 2577.py

--- code_practice_sites/leetcode/2k/2k5/2577.py
+++ code_practice_sites/leetcode/2k/2k5/2577.py
@@ -1,29 +1,73 @@
+import heapq
+from collections import defaultdict
 from typing import *
 import inspect
 
 
 class Solution:
-    """
-    Solulu.
-    """
-
     def __init__(self):
-        self.test = "test"
+        self.grid = [[0]]
+        self.END=0,0
 
-    def Template(self, L: List, i: int):
-        return self.test
+    def addEl(self, i, j, v):
+        if v in self.PQD:
+            self.PQD[v].add((i, j))
+        else:
+            heapq.heappush(self.PQ, v)
+            self.PQD[v] = {(i, j)}
+        self.grid[i][j] = ~v
 
-    main = Template
+    def getNext(self, i, j, nextime):
+        offset = ((i + j) & 1) ^ 1
+        X=[]
+        if i>0:
+            X.append((i-1,j))
+        if i<self.END[0]:
+            X.append((i+1,j))
+        if j>0:
+            X.append((i,j-1))
+        if j<self.END[1]:
+            X.append((i,j+1))
+        for nex_i, nex_j in X:
+            minimum = self.grid[nex_i][nex_j]
+            if minimum < 0:
+                continue
+            if minimum < nextime:
+                minimum = nextime
+            elif minimum & 1 != offset:
+                minimum += 1
+
+            self.addEl(nex_i, nex_j, minimum)
+        return
+
+    def minimumTime(self, grid: List[List[int]]) -> int:
+        if min(grid[0][1], grid[1][0]) > 1:
+            return -1
+        self.grid = grid
+        self.PQ: list[int] = []
+        self.PQD = {}
+        self.addEl(0, 0, 0)
+        self.END = len(self.grid)-1,len(self.grid[0])-1
+        while self.PQ:
+            curtime = heapq.heappop(self.PQ)
+            curvals = self.PQD.pop(curtime)
+            for E in curvals:
+                if E == self.END:
+                    return curtime
+                self.getNext(*E, curtime + 1)
+        return -1
+
+    main = minimumTime
 
 
 TESTS = [
     (
-        ([0, 1], 1),
-        "test")
+        ([[0, 1, 3, 2], [5, 1, 2, 5], [4, 3, 8, 6]],),
+        7)
     ,
     (
-        ([0, 1], 2),
-        "also test"
+        ([[0, 7], [7, 2]],),
+        -1
     )
 ]
 
